d8f3e07698346c993ebcfa947e9606fe7dd65d3a
MadsRantzau49 <116832024+MadsRantzau49@users.noreply.github.com>
2025-02-07 23:49:46 +0100

    relative database_location

--- Database/database_setup.py
+++ Database/database_setup.py
@@ -1,10 +1,17 @@
 from sqlalchemy import create_engine, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 Base = declarative_base()
 
-DATABASE_URL = 'sqlite:///Database/database.db'
+Base = declarative_base()
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Gets the absolute path of the script's directory
+DATABASE_PATH = os.path.join(BASE_DIR, "database.db")
+
+DATABASE_URL = f'sqlite:///{DATABASE_PATH}'
+
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
